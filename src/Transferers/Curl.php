1b834695b694aa4545d6667174b351616b3e9f59
midnightelf <abdulovk20@gmail.com>
2022-06-19 00:25:23 +0300

    Add flexible headers for Curl request

--- src/Transferers/Curl.php
+++ src/Transferers/Curl.php
@@ -3,43 +3,73 @@
 namespace FincanaTest\Transferers;
 
 use CurlHandle;
+use FincanaTest\Helpers\ArrayHelper;
 use FincanaTest\Interfaces\TransfererInterface;
 
 class Curl implements TransfererInterface
 {
-    protected readonly CurlHandle $curl;
+    protected CurlHandle $curl;
+    private array $headers;
 
     public function __construct()
     {
         $this->curl = curl_init();
+        $this->headers = [];
 
         $this->setDefaultOpts();
+    }
+
+    public function getCurl()
+    {
+        return $this->curl;
     }
 
     public function get()
     {
         $this->close();
 
-        return json_decode(curl_exec($this->curl));
+        $res = json_decode(curl_exec($this->curl));
+
+        return $this->convertResponse($res);
     }
 
-    public function post(array $args = [])
+    public function post($args)
     {
         $this->close();
 
         $this->setOption(CURLOPT_POSTFIELDS, $args);
 
-        return json_decode(curl_exec($this->curl));
+        $res = json_decode(curl_exec($this->curl));
+
+        $this->convertResponse($res);
     }
 
-    public function setOption(int $option, mixed $value)
+    public function setOption(int $option, mixed $value): void
     {
         curl_setopt($this->curl, $option, $value);
     }
 
-    public function setHeader(string $header, string $val)
+    public function setHeader(string $header, string $val): void
     {
-        $this->setOption(CURLOPT_HTTPHEADER, array($header, $val));
+        $header = trim($header);
+        $val = trim($val);
+
+        $this->unsetHeader($header);
+
+        $header = "$header: $val";
+
+        array_push($this->headers, $header);
+
+        $this->setOption(CURLOPT_HTTPHEADER, $this->headers);
+    }
+
+    public function unsetHeader(string $header): void
+    {
+        $idx = ArrayHelper::startsWith($header, $this->headers);
+
+        if ($idx !== false) {
+            array_splice($this->headers, $idx, 1);
+        }
     }
 
     public function setUrl(string $url): self
@@ -49,14 +79,23 @@ class Curl implements TransfererInterface
         return $this;
     }
 
-    public function close()
+    protected function close()
     {
         curl_close($this->curl);
+    }
+
+    private function convertResponse($response)
+    {
+        if (is_object($response)) {
+            return get_object_vars($response);
+        }
+
+        return $response;
     }
 
     private function setDefaultOpts()
     {
         $this->setOption(CURLOPT_RETURNTRANSFER, true);
-        $this->setOption(CURLOPT_HEADER, false);
+        $this->setHeader('Content-Type', 'application/json');
     }
 }
